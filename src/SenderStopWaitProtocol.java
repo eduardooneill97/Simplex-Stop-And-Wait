10f5c06b6e69ffbe3b703661c7a7d0ba7ad397e1
Anibal Pagan <anibal.pagan@upr.edu>
2020-03-21 22:02:12 -0400

    Added Stop and Wait Protocol layer for sender and receiver.

---
+++ src/SenderStopWaitProtocol.java
@@ -0,0 +1,89 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.LinkedBlockingQueue;
+import javax.swing.Timer;
+
+public class SenderStopWaitProtocol {
+	
+	private LinkedBlockingQueue<Frame> messageQueue;
+	private Frame current; //last message sent
+	private Frame acknowledgement;
+	private Thread sendLoop;
+	private int sequence = 0;
+	private Object lock;
+	private CommunicationLayer c;
+	private Timer t = new Timer(1000, new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(acknowledgement == null) {
+				//for testing
+				System.out.println("No acknowledgement received!");
+				System.out.println("Messages in queue: " + messageQueue.size()); // +1 for frame in current
+				c.send(current);
+				t.restart();
+				t.start();
+			}
+		}
+	});
+	
+	public SenderStopWaitProtocol(int source, String destAddress, int destPort) {
+		messageQueue = new LinkedBlockingQueue<>();
+		c = new CommunicationLayer(source, destAddress, destPort);
+		lock = new Object();
+		c.start();
+		sendLoop = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					while(true) {
+						synchronized (lock) {
+							if(t.isRunning()) lock.wait();
+							send();
+						}
+					}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		});
+		c.receive(new ReceiveCallback() {
+			@Override
+			public void onReceive(Frame frame) {
+				synchronized (lock) {
+					acknowledgement = frame;
+					//for testing purposes!
+					System.out.println("Acknowledgement of message: " + acknowledgement.getAck());
+					t.stop();
+					lock.notify();
+				}
+			}
+		});
+	}
+	
+	public void enqueue(String message) {
+		Frame f = new Frame();
+		f.setData(message.getBytes());
+		messageQueue.add(f);
+	}
+	
+	public void start() {
+		sendLoop.start();
+	}
+	
+	private void send() {
+		try {
+			current = messageQueue.take();
+			current.setSeq(sequence);
+			sequence = (sequence == 0) ? 1 : 0;
+			acknowledgement = null;
+			c.send(current);
+			t.start();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
