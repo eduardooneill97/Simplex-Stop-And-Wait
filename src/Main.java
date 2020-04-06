6268cadde61bef4d46e2f541ae229942a186636f
Roberto Rivera <roberto.rivera16@upr.edu>
2020-04-06 12:32:57 -0400

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,44 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		//tests
+		Scanner scanner = new Scanner(System.in);
+		int input = 0;
+		String message = "";
 		SenderStopWaitProtocol s = new SenderStopWaitProtocol(3000, "127.0.0.1", 5000);
 		ReceiverStopWaitProtocol r = new ReceiverStopWaitProtocol(5000, "127.0.0.1", 3000);
-		
-		s.start();
-		r.receive(new MessageCallback() {
-			
-			@Override
-			public void onReceive(String message) {
-				// TODO Auto-generated method stub
-				System.out.println("Received: "+message);
+
+		System.out.println("Two Layer Communication System\n"); 
+		while(input == 0 || input != 1 || input != 2){
+			System.out.println("Are you the (1) Receiver or (2) Sender?");
+			input = Integer.parseInt(scanner.nextLine());
+			if(input != 1 || input != 2){
+				System.out.println("Invalid argument. Try again.\n");
 			}
-		});
-		
-		for(int i = 0; i<20; i++) {
-			s.enqueue("Message " + i + ".");
 		}
+
+		if(input == 1){
+			System.out.println("*** RECEIVER TERMINAL ***\n");
+			while(true){
+				r.receive(new MessageCallback() {
+			
+					@Override
+					public void onReceive(String message) {
+						System.out.println("Received: "+ message);
+					}
+				});
+			}
+		}else{
+			s.start();
+			
+			System.out.println("*** SENDER TERMINAL ***\n");
+			while(true){
+				System.out.println("Write message: ");
+				message = scanner.nextLine();
+				s.enqueue(message);
+			}
+
+		}
+		
 	}
 }
