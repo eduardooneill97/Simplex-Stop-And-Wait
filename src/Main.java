df8a6ef55a70cf4443aa142d3c002d66713075fd
Roberto Rivera <roberto.rivera16@upr.edu>
2020-04-06 12:43:40 -0400

    Comment

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int input = 0;
+		String input = "";
 		String message = "";
 		SenderStopWaitProtocol s = new SenderStopWaitProtocol(3000, "127.0.0.1", 5000);
 		ReceiverStopWaitProtocol r = new ReceiverStopWaitProtocol(5000, "127.0.0.1", 3000);
 
 		System.out.println("Two Layer Communication System\n"); 
-		while(input == 0 || input != 1 || input != 2){
+		while(input.equals("1") || input.equals("2")){
 			System.out.println("Are you the (1) Receiver or (2) Sender?");
-			input = Integer.parseInt(scanner.nextLine());
-			if(input != 1 || input != 2){
+			input = scanner.nextLine();
+			if(input.equals("1") || input.equals("2")){
 				System.out.println("Invalid argument. Try again.\n");
 			}
 		}
 
-		if(input == 1){
+		if(input.equals("1")){
 			System.out.println("*** RECEIVER TERMINAL ***\n");
 			while(true){
 				r.receive(new MessageCallback() {
@@ -30,7 +30,6 @@ public class Main {
 			}
 		}else{
 			s.start();
-			
 			System.out.println("*** SENDER TERMINAL ***\n");
 			while(true){
 				System.out.println("Write message: ");
