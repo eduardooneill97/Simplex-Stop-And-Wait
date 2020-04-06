a113e4a4a895c5d56b360c48e1a1152bb68cd1b1
Roberto Rivera <roberto.rivera16@upr.edu>
2020-04-06 13:27:27 -0400

    IP Address Input

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,41 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		String input = "";
 		String message = "";
-		SenderStopWaitProtocol s = new SenderStopWaitProtocol(3000, "127.0.0.1", 5000);
-		ReceiverStopWaitProtocol r = new ReceiverStopWaitProtocol(5000, "127.0.0.1", 3000);
+		SenderStopWaitProtocol s;
+		ReceiverStopWaitProtocol r;
 
 		System.out.println("Two Layer Communication System\n"); 
 		while(!(input.equals("1") || input.equals("2"))){
 			System.out.println("Are you the (1) Receiver or (2) Sender?");
 			input = scanner.nextLine();
 			if(!(input.equals("1") || input.equals("2"))){
-				System.out.println("Invalid argument. Try again.\n");
+				System.out.println("Invalid option. Try again.\n");
 			}
 		}
 
 		if(input.equals("1")){
+			String receiver_ip = "";
 			System.out.println("*** RECEIVER TERMINAL ***\n");
-			while(true){
-				r.receive(new MessageCallback() {
-			
-					@Override
-					public void onReceive(String message) {
-						System.out.println("Received: "+ message);
-					}
-				});
-			}
+			System.out.println("(Local: 127.0.0.1) Enter Receiver IP Address:");
+
+			receiver_ip = scanner.nextLine();
+			r = new ReceiverStopWaitProtocol(5000, receiver_ip, 3000);
+
+			r.receive(new MessageCallback() {
+				@Override
+				public void onReceive(String message) {
+					System.out.println("Received: "+ message);
+				}
+			});
 		}else{
-			s.start();
+			String sender_ip = "";
 			System.out.println("*** SENDER TERMINAL ***\n");
+			System.out.println("(Local: 127.0.0.1) Enter Sender IP Address:");
+			
+			sender_ip = scanner.nextLine();
+			s = new SenderStopWaitProtocol(3000, sender_ip, 5000);
+
+			s.start();
 			while(true){
 				System.out.println("Write message: ");
 				message = scanner.nextLine();
